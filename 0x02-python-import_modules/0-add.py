fd31d29777e37eba0bcb9287376deb0300dcd09c
ajibad-coder <meriesbells1996@gmail.com>
2023-10-05 05:11:27 -0700

    add_update

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
-answer = add(a,b)
-print(f"{a} + {b} = {answer}")
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print(f"{a} + {b} = {add(a,b)}")
